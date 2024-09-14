e0a76a4b20ae523c34af09094a6bbc632b88898a
crosh92 <jonathanvisconti1992@gmail.com>
2024-09-14 09:41:57 -0500

    requerimiento completado

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 def sumar(a, b):
     # aqui se realiza la suma de dos numeros
-
+    return (a+b)
 def restar(a, b):
     # aqui se realiza la resta de dos numeros
-
+    return (a-b)
 def multiplicar(a, b):
     # aqui se realiza la multiplicacion de dos numeros
-
+    return (a*b)
 def dividir(a, b):
     # aqui se realiza la division de dos numeros
-
+    if b == 0:
+        return "Error: No se puede dividir por cero"
+    return (a/b)
 def main():
     num1 = float(input("Ingrese el primer número: "))
     num2 = float(input("Ingrese el segundo número: "))
